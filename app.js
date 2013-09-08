c6439ae23010459497ba0b5e839107fd67ccf956
jsownz <jason@frozensoliddesigns.com>
2013-09-08 15:06:11 -0700

    pulling redis

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     fs      = require('fs'),
-    redis   = require('redis'),
-    client  = redis.createClient(),
+    //redis   = require('redis'),
+    //client  = redis.createClient(),
     app     = express();
 
 var hbs     = require('hbs');
@@ -12,46 +12,46 @@ app.engine('html', hbs.__express);
 app.use(express.bodyParser());
 app.use(express.static('assets'));
 
-client.on("error", function(err){
-  console.log("Error: "+err);
-});
+//client.on("error", function(err){
+//  console.log("Error: "+err);
+//});
 
 app.get('/',function(req, res){
-  client.get("username", function(err, reply){
-    if ( err || reply === null ) {
-      res.render('welcome', {
-        title:"Welcome",
-        icon:"user",
-        javascripts:['welcome.js']
-      });
-    } else {
-      res.render('hello', {
-        title: "Hello!", 
-        icon: "user",
-        username: reply.toString()
-      });
-    }
-  });
+//  client.get("username", function(err, reply){
+//    if ( err || reply === null ) {
+//      res.render('welcome', {
+//        title:"Welcome",
+//        icon:"user",
+//        javascripts:['welcome.js']
+//      });
+//    } else {
+//      res.render('hello', {
+//        title: "Hello!", 
+//        icon: "user",
+//        username: reply.toString()
+//      });
+//    }
+//  });
 });
 app.get('/about',function(req, res){
   res.render('about', {title:"About"});
-  client.del("username");
+//  client.del("username");
 });
 
 app.post('/user', function(req, res){
   //set username
   //client.set("username", req.body.username);
-  var user = { username: req.body.username, password: req.body.password };
-  
-  client.hmset('users.'+req.body.username, user);
+//  var user = { username: req.body.username, password: req.body.password };
+//  
+//  client.hmset('users.'+req.body.username, user);
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('success');
 });
 
 app.get('/users/', function(req, res){
-  client.hkeys('users', function(err, replies) {
-    console.log(replies);
-  });
+//  client.hkeys('users', function(err, replies) {
+//    console.log(replies);
+//  });
 });
 
 var port = process.env.PORT || 5000;
